a0da3849b69ede631740b2c3cddcb2c6c37c072d
Eivydas Senkus <eivyses@gmail.com>
2019-05-02 23:12:27 +0300

    Added extra moves for player so he could move diagonal

--- core/src/com/mygdx/bomberman/Bomberman.java
+++ core/src/com/mygdx/bomberman/Bomberman.java
@@ -19,134 +19,158 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Bomberman extends ApplicationAdapter {
-    private Texture bluePlayer;
-    private SpriteBatch batch;
-    private Socket socket;
-    private Player player;
-    private Map<String, Player> players;
-    private FrameRate frameRate;
-    private Level level;
+  private Texture bluePlayer;
+  private SpriteBatch batch;
+  private Socket socket;
+  private Player player;
+  private Map<String, Player> players;
+  private FrameRate frameRate;
+  private Level level;
 
-    @Override
-    public void create() {
-        batch = new SpriteBatch();
-        bluePlayer = new Texture("textures\\Bomberman.png");
-        frameRate = new FrameRate(batch);
-        players = new HashMap<>();
-        level = new Level(batch);
-        connectSocket();
-        configSocketEvents();
+  @Override
+  public void create() {
+    batch = new SpriteBatch();
+    bluePlayer = new Texture("textures\\Bomberman.png");
+    frameRate = new FrameRate(batch);
+    players = new HashMap<>();
+    level = new Level(batch);
+    connectSocket();
+    configSocketEvents();
+  }
+
+  @Override
+  public void render() {
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+    if (player != null) {
+      player.setMoved(false);
+    }
+    float dt = Gdx.graphics.getDeltaTime();
+    if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+      move(player.getX(), player.getY() + dt * player.getSpeed());
+    }
+    if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+      move(player.getX(), player.getY() - dt * player.getSpeed());
+    }
+    if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+      move(player.getX() - dt * player.getSpeed(), player.getY());
+    }
+    if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+      move(player.getX() + dt * player.getSpeed(), player.getY());
+    }
+    if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && Gdx.input.isKeyPressed(Input.Keys.UP)) {
+      move(player.getX() + dt * player.getSpeed(), player.getY() + dt * player.getSpeed());
+    }
+    if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+      move(player.getX() + dt * player.getSpeed(), player.getY() - dt * player.getSpeed());
+    }
+    if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && Gdx.input.isKeyPressed(Input.Keys.UP)) {
+      move(player.getX() - dt * player.getSpeed(), player.getY() + dt * player.getSpeed());
+    }
+    if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+      move(player.getX() - dt * player.getSpeed(), player.getY() - dt * player.getSpeed());
+    }
+    if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+      placeBomb(player.getX(), player.getY());
     }
 
-    @Override
-    public void render() {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        if (player != null) {
-            player.setMoved(false);
-        }
-        float dt = Gdx.graphics.getDeltaTime();
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            move(player.getX(), player.getY() + dt * player.getSpeed());
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            move(player.getX(), player.getY() - dt * player.getSpeed());
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            move(player.getX() - dt * player.getSpeed(), player.getY());
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            move(player.getX() + dt * player.getSpeed(), player.getY());
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
-            placeBomb(player.getX(), player.getY());
-        }
-
-        batch.begin();
-        if (level != null) {
-            level.draw();
-        }
-        frameRate.update();
-        frameRate.draw();
-        for (Map.Entry<String, Player> p : players.entrySet()) {
-            p.getValue().draw();
-        }
-        batch.end();
+    batch.begin();
+    if (level != null) {
+      level.draw();
     }
-
-    private void move(float x, float y) {
-        MoveDto moveDto = new MoveDto(x, y);
-        socket.emit("move", moveDto.toJson());
+    frameRate.update();
+    frameRate.draw();
+    for (Map.Entry<String, Player> p : players.entrySet()) {
+      p.getValue().draw();
     }
+    batch.end();
+  }
 
-    private void placeBomb(float x, float y) {
-        MoveDto moveDto = new MoveDto(x, y);
-        socket.emit("bombPlaced", moveDto.toJson());
-    }
+  private void move(float x, float y) {
+    MoveDto moveDto = new MoveDto(x, y);
+    socket.emit("move", moveDto.toJson());
+  }
 
-    @Override
-    public void dispose() {
-        batch.dispose();
-        PlayerDto playerDto = new PlayerDto(player);
-        socket.emit("disconnected", playerDto.toJson());
-    }
+  private void placeBomb(float x, float y) {
+    MoveDto moveDto = new MoveDto(x, y);
+    socket.emit("bombPlaced", moveDto.toJson());
+  }
 
-    private void connectSocket() {
-        try {
-            socket = IO.socket(Constants.url + ":" + Constants.port);
-            socket.connect();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-    }
+  @Override
+  public void dispose() {
+    batch.dispose();
+    PlayerDto playerDto = new PlayerDto(player);
+    socket.emit("disconnected", playerDto.toJson());
+  }
 
-    private void configSocketEvents() {
-        // connect
-        socket.on(Socket.EVENT_CONNECT, args -> {
-            System.out.println("Socket connected");
-            socket.emit("connection", "me");
-            System.out.println("Socket emitted");
-        })
-                // new user connected
-                .on("newPlayerConnected", args -> {
-                    PlayerDto playerDto = new Gson().fromJson(args[0].toString(), PlayerDto.class);
-                    System.out.println("Player connected: " + playerDto.getId());
-                    Player p = new Player(playerDto, bluePlayer, batch);
-                    players.put(p.getId(), p);
-                })
-                // get player object for new player
-                .on("user", args -> {
-                    PlayerDto playerDto = new Gson().fromJson(args[0].toString(), PlayerDto.class);
-                    player = new Player(playerDto, bluePlayer, batch);
-                    players.put(playerDto.getId(), player);
-                })
-                // get other players for new player
-                .on("otherPlayers", args -> {
-                    PlayersDto playersDto = new Gson().fromJson(args[0].toString(), PlayersDto.class);
-                    players.clear();
-                    for (Map.Entry<String, PlayerDto> p : playersDto.getPlayers().entrySet()) {
-                        Player pl = new Player(p.getValue(), bluePlayer, batch);
-                        players.put(pl.getId(), pl);
-                    }
-                })
-                // get map for new player
-                .on("map", args -> {
-                    LevelDto levelDto = new Gson().fromJson(args[0].toString(), LevelDto.class);
-                    level.setLevel(levelDto);
-                })
-                // get player moved
-                .on("playerMoved", args -> {
-                    PlayerDto playerDto = new Gson().fromJson(args[0].toString(), PlayerDto.class);
-                    Player p = players.get(playerDto.getId());
-                    if (p == null) {
-                        p = new Player(playerDto, bluePlayer, batch);
-                    }
-                    // hack
-                    if (player.getId().equals(p.getId())) {
-                        player = p;
-                    }
-                    p.setX(playerDto.getX());
-                    p.setY(playerDto.getY());
-                });
+  private void connectSocket() {
+    try {
+      socket = IO.socket(Constants.url + ":" + Constants.port);
+      socket.connect();
+    } catch (URISyntaxException e) {
+      e.printStackTrace();
     }
+  }
+
+  private void configSocketEvents() {
+    // connect
+    socket
+        .on(
+            Socket.EVENT_CONNECT,
+            args -> {
+              System.out.println("Socket connected");
+              socket.emit("connection", "me");
+              System.out.println("Socket emitted");
+            })
+        // new user connected
+        .on(
+            "newPlayerConnected",
+            args -> {
+              PlayerDto playerDto = new Gson().fromJson(args[0].toString(), PlayerDto.class);
+              System.out.println("Player connected: " + playerDto.getId());
+              Player p = new Player(playerDto, bluePlayer, batch);
+              players.put(p.getId(), p);
+            })
+        // get player object for new player
+        .on(
+            "user",
+            args -> {
+              PlayerDto playerDto = new Gson().fromJson(args[0].toString(), PlayerDto.class);
+              player = new Player(playerDto, bluePlayer, batch);
+              players.put(playerDto.getId(), player);
+            })
+        // get other players for new player
+        .on(
+            "otherPlayers",
+            args -> {
+              PlayersDto playersDto = new Gson().fromJson(args[0].toString(), PlayersDto.class);
+              players.clear();
+              playersDto
+                  .getPlayers()
+                  .forEach((key, value) -> players.put(key, new Player(value, bluePlayer, batch)));
+            })
+        // get map for new player
+        .on(
+            "map",
+            args -> {
+              LevelDto levelDto = new Gson().fromJson(args[0].toString(), LevelDto.class);
+              level.setLevel(levelDto);
+            })
+        // get player moved
+        .on(
+            "playerMoved",
+            args -> {
+              PlayerDto playerDto = new Gson().fromJson(args[0].toString(), PlayerDto.class);
+              Player p = players.get(playerDto.getId());
+              if (p == null) {
+                p = new Player(playerDto, bluePlayer, batch);
+              }
+              // hack
+              if (player.getId().equals(p.getId())) {
+                player = p;
+              }
+              p.setX(playerDto.getX());
+              p.setY(playerDto.getY());
+            });
+  }
 }
--- core/src/com/mygdx/bomberman/entities/Player.java
+++ core/src/com/mygdx/bomberman/entities/Player.java
@@ -6,111 +6,111 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.bomberman.dto.PlayerDto;
 
 public class Player {
-    private float x;
-    private float y;
-    private int maxWidth;
-    private int maxHeight;
-    private String id;
-    private boolean moved;
+  private float x;
+  private float y;
+  private int maxWidth;
+  private int maxHeight;
+  private String id;
+  private boolean moved;
 
-    private Texture texture;
-    private SpriteBatch batch;
+  private Texture texture;
+  private SpriteBatch batch;
 
-    private float speed;
+  private float speed;
 
-    public Player(PlayerDto playerDto, Texture texture, SpriteBatch batch) {
-        this.texture = texture;
-        this.id = playerDto.getId();
-        x = playerDto.getX();
-        y = playerDto.getY();
-        speed = 120.0f;
-        this.batch = batch;
+  public Player(PlayerDto playerDto, Texture texture, SpriteBatch batch) {
+    this.texture = texture;
+    this.id = playerDto.getId();
+    x = playerDto.getX();
+    y = playerDto.getY();
+    speed = 120.0f;
+    this.batch = batch;
 
-        maxWidth = Gdx.graphics.getWidth() - texture.getWidth();
-        maxHeight = Gdx.graphics.getHeight() - texture.getHeight();
+    maxWidth = Gdx.graphics.getWidth() - texture.getWidth();
+    maxHeight = Gdx.graphics.getHeight() - texture.getHeight();
+  }
+
+  public boolean isMoved() {
+    return moved;
+  }
+
+  public void setMoved(boolean moved) {
+    this.moved = moved;
+  }
+
+  public int getMaxWidth() {
+    return maxWidth;
+  }
+
+  public int getMaxHeight() {
+    return maxHeight;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public Texture getTexture() {
+    return texture;
+  }
+
+  public SpriteBatch getBatch() {
+    return batch;
+  }
+
+  public void draw() {
+    batch.draw(texture, x, y);
+  }
+
+  public float getX() {
+    return x;
+  }
+
+  public void setX(float x) {
+    this.x = x;
+  }
+
+  public float getY() {
+    return y;
+  }
+
+  public void setY(float y) {
+    this.y = y;
+  }
+
+  public void moveUp(float dt) {
+    y += dt * speed;
+    if (y > maxHeight) {
+      y = maxHeight;
     }
+  }
 
-    public boolean isMoved() {
-        return moved;
+  public void moveDown(float dt) {
+    y -= dt * speed;
+    if (y < 0) {
+      y = 0.0f;
     }
+  }
 
-    public void setMoved(boolean moved) {
-        this.moved = moved;
+  public void moveRight(float dt) {
+    x += dt * speed;
+    if (x > maxWidth) {
+      x = maxWidth;
     }
+  }
 
-    public int getMaxWidth() {
-        return maxWidth;
+  public void moveLeft(float dt) {
+    x -= dt * speed;
+    if (x < 0) {
+      x = 0.0f;
     }
+  }
 
-    public int getMaxHeight() {
-        return maxHeight;
-    }
+  public float getSpeed() {
+    return speed;
+  }
 
-    public String getId() {
-        return id;
-    }
-
-    public Texture getTexture() {
-        return texture;
-    }
-
-    public SpriteBatch getBatch() {
-        return batch;
-    }
-
-    public void draw() {
-        batch.draw(texture, x, y);
-    }
-
-    public float getX() {
-        return x;
-    }
-
-    public void setX(float x) {
-        this.x = x;
-    }
-
-    public float getY() {
-        return y;
-    }
-
-    public void setY(float y) {
-        this.y = y;
-    }
-
-    public void moveUp(float dt) {
-        y += dt * speed;
-        if (y > maxHeight) {
-            y = maxHeight;
-        }
-    }
-
-    public void moveDown(float dt) {
-        y -= dt * speed;
-        if (y < 0) {
-            y = 0.0f;
-        }
-    }
-
-    public void moveRight(float dt) {
-        x += dt * speed;
-        if (x > maxWidth) {
-            x = maxWidth;
-        }
-    }
-
-    public void moveLeft(float dt) {
-        x -= dt * speed;
-        if (x < 0) {
-            x = 0.0f;
-        }
-    }
-
-    public float getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(float speed) {
-        this.speed = speed;
-    }
+  public void setSpeed(float speed) {
+    this.speed = speed;
+  }
 }
