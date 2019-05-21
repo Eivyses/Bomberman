5b454cea357d5f6e732e87eb929a15945a85b265
Zilvinas Abromavicius <abromavicius.zilvinas@gmail.com>
2019-05-21 14:16:22 +0300

    Test commit.

---
+++ server/src/main/java/com/bomberman/zilvinasGame/Game.java
@@ -0,0 +1,15 @@
+package com.bomberman.zilvinasGame;
+
+import com.bomberman.entities.Position;
+
+public class Game {
+    Player player;
+
+    public void movePlayer(int playerId, Position position) {
+
+    }
+
+    public void placeBomb(Position position) {
+
+    }
+}
