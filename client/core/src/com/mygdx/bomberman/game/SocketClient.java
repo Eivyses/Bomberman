7cae6273297d8f582524e7bf76e638253c3ba206
Eivydas Senkus <eivydas@satalia.com>
2019-06-06 12:56:53 +0300

    cleanup

--- client/core/src/com/mygdx/bomberman/game/SocketClient.java
+++ client/core/src/com/mygdx/bomberman/game/SocketClient.java
@@ -5,7 +5,6 @@ import com.mygdx.bomberman.constants.Constants;
 import com.mygdx.bomberman.entities.Position;
 import io.socket.client.IO;
 import io.socket.client.Socket;
-
 import java.net.URISyntaxException;
 
 public class SocketClient {
@@ -62,70 +61,5 @@ public class SocketClient {
               final GameState gameState = new Gson().fromJson(args[0].toString(), GameState.class);
               game.setGameState(gameState);
             });
-    // get player object for new player
-    //                .on(
-    //                        "user",
-    //                        args -> {
-    //                            PlayerDto playerDto = new Gson().fromJson(args[0].toString(),
-    // PlayerDto.class);
-    //                            player = new Player(playerDto, bluePlayer, batch);
-    //                            players.put(playerDto.getId(), player);
-    //                        })
-    //                // get other players for new player
-    //                .on(
-    //                        "otherPlayers",
-    //                        args -> {
-    //                            PlayersDto playersDto = new Gson().fromJson(args[0].toString(),
-    // PlayersDto.class);
-    //                            players.clear();
-    //                            playersDto
-    //                                    .getPlayers()
-    //                                    .forEach((key, value) -> players.put(key, new
-    // Player(value, bluePlayer, batch)));
-    //                        })
-    //                // get map for new player
-    //                .on(
-    //                        "map",
-    //                        args -> {
-    //                            LevelDto levelDto = new Gson().fromJson(args[0].toString(),
-    // LevelDto.class);
-    //                            level.setLevel(levelDto);
-    //                        })
-    //                // bomb placed
-    //                .on(
-    //                        "bombPlaced",
-    //                        args -> {
-    //                            final BombsDto bombsDto = new Gson().fromJson(args[0].toString(),
-    // BombsDto.class);
-    //                            bombs.clear();
-    //                            bombsDto.getBombs().forEach(bomb -> bombs.add(new Bomb(bomb,
-    // bombTexture, batch)));
-    //                        })
-    //                // bomb exploded
-    //                .on(
-    //                        "bombExploded",
-    //                        args -> {
-    //                            final BombsDto bombsDto = new Gson().fromJson(args[0].toString(),
-    // BombsDto.class);
-    //                            bombs.clear();
-    //                            bombsDto.getBombs().forEach(bomb -> bombs.add(new Bomb(bomb,
-    // bombTexture, batch)));
-    //                        })
-    //                // get player moved
-    //                .on(
-    //                        "playerMoved",
-    //                        args -> {
-    //                            final PlayerDto playerDto = new
-    // Gson().fromJson(args[0].toString(), PlayerDto.class);
-    //                            Player p = players.get(playerDto.getId());
-    //                            if (p == null) {
-    //                                p = new Player(playerDto, bluePlayer, batch);
-    //                            }
-    //                            // hack
-    //                            if (player.getId().equals(p.getId())) {
-    //                                player = p;
-    //                            }
-    //                            p.setPosition(playerDto.getPosition());
-    //                        });
   }
 }
