298e5b8ea7931b323eb6e289b8756854d7e6c63a
Zilvinas Abromavicius <abromavicius.zilvinas@gmail.com>
2019-08-02 14:07:42 +0300

    init phaser client and node server

---
+++ node-server/index.js
@@ -0,0 +1,18 @@
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, () => {
+  console.log("listening on *:3000");
+});
